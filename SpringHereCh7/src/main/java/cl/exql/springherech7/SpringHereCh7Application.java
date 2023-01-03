ebbd46b112b494abae848a08ffa59d281dfb230e
Exequiel Venegas <107861898+Evenegas4414@users.noreply.github.com>
2023-01-03 11:35:36 -0300

    Add files via upload

---
+++ SpringHereCh7/src/main/java/cl/exql/springherech7/SpringHereCh7Application.java
@@ -0,0 +1,13 @@
+package cl.exql.springherech7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHereCh7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHereCh7Application.class, args);
+    }
+
+}
---
+++ SpringHereCh7/src/test/java/cl/exql/springherech7/SpringHereCh7ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.exql.springherech7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHereCh7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
