77d3fff50b35949f6bfa531b035a21a5a89eba3e
Exequiel Venegas <107861898+Evenegas4414@users.noreply.github.com>
2023-01-10 23:03:44 -0300

    Add files via upload

---
+++ SpringHere11Payments/src/main/java/cl/exql/springhere11payments/SpringHere11PaymentsApplication.java
@@ -0,0 +1,13 @@
+package cl.exql.springhere11payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHere11PaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHere11PaymentsApplication.class, args);
+    }
+
+}
---
+++ SpringHere11Payments/src/test/java/cl/exql/springhere11payments/SpringHere11PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.exql.springhere11payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHere11PaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
