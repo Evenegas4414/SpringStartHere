f224bddc1e9a84cfa27df89aa175448c169bfa94
Exequiel Venegas <107861898+Evenegas4414@users.noreply.github.com>
2023-01-14 12:02:18 -0300

    Add files via upload

---
+++ SpringHere13/src/main/java/cl/exql/SpringHere13Application.java
@@ -0,0 +1,13 @@
+package cl.exql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHere13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHere13Application.class, args);
+    }
+
+}
---
+++ SpringHere13/target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO account VALUES (NULL, 'Hellen Down', 1000);
+INSERT INTO account VALUES (NULL, 'Peter Read', 2000);
